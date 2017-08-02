204831e6501d49cf2cccb14cdbf4a80ce3f717b8
hungrymonkey <boringmonkey@gmail.com>
2017-08-02 15:50:49 -0700

    yey it compiles. Of course it fails, but it compiles. Yeyyyy

--- android/src/main/java/com/yourcompany/micstream/MicStreamPlugin.java
+++ android/src/main/java/com/yourcompany/micstream/MicStreamPlugin.java
@@ -4,26 +4,91 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import android.media.AudioRecord;
+import android.media.AudioRecord.OnRecordPositionUpdateListener;
+import android.media.AudioFormat;
+import android.media.MediaRecorder;
+import android.util.Log;
+import android.app.Activity;
 /**
  * MicStreamPlugin
  */
-public class MicStreamPlugin implements MethodCallHandler {
+public class MicStreamPlugin implements EventChannel.StreamHandler {
   /**
    * Plugin registration.
    */
+  private static final String MIC_STREAM_CHANNEL = "com.yourcompany.micstream/record";
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "mic_stream");
-    channel.setMethodCallHandler(new MicStreamPlugin());
+    final EventChannel micChannel = new EventChannel(registrar.messenger(), MIC_STREAM_CHANNEL);
+    micChannel.setStreamHandler(new MicStreamPlugin(
+            registrar.activity()));
   }
-
+  private final String LOG_TAG = "mic record";
+  private AudioRecord mRecorder;
+  private OnRecordPositionUpdateListener mListener;
+  private int mInBufferSize;
+  private int mSampleRate;
+  private int mPeriodFrames;
+  private short [] audioData;
+  private int reads;
+  private static int mFormat = AudioFormat.ENCODING_PCM_16BIT;
+  private MicStreamPlugin(Activity activity){
+    mRecorder = null;
+    mSampleRate = 48000;
+    mInBufferSize = AudioRecord.getMinBufferSize(mSampleRate, AudioFormat.CHANNEL_IN_MONO, mFormat);
+    //https://stackoverflow.com/questions/15804903/android-dev-audiorecord-without-blocking-or-threads
+    //require android 5
+    mPeriodFrames = mInBufferSize / 2;
+    audioData = new short [mPeriodFrames];
+  }
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+  public void onListen(Object arguments, EventChannel.EventSink events) {
+
+    mRecorder = new AudioRecord(
+            MediaRecorder.AudioSource.MIC,
+            mSampleRate, AudioFormat.CHANNEL_IN_MONO,
+            mFormat, mInBufferSize
+    );
+
+
+    mListener = createRecordListener(events);
+    mRecorder.setRecordPositionUpdateListener(mListener);
+    mRecorder.setPositionNotificationPeriod(mPeriodFrames);
+    try {
+      mRecorder.startRecording();
+    } catch (IllegalStateException e) {
+      Log.e(LOG_TAG, "record() failed");
+    }
+    reads = 0;
+  }
+  @Override
+  public void onCancel(Object args) {
+    if (mRecorder != null) {
+      mRecorder.stop();
+      mRecorder.release();
+      mRecorder = null;
     }
   }
+  OnRecordPositionUpdateListener createRecordListener(final EventChannel.EventSink events){
+    return new OnRecordPositionUpdateListener(){
+      public void onMarkerReached(AudioRecord recorder){
+        int bytesOut = recorder.read(audioData,0,mInBufferSize);
+        Log.e(LOG_TAG, "marker reached " + reads );
+      }
+      public void onPeriodicNotification(AudioRecord recorder){
+        int bytesOut = recorder.read(audioData,0,mInBufferSize);
+        short[] audioValues = new short[audioData.length];
+        //make a copy of values to pass
+        for(int i = 0; i< audioData.length; i++){
+          audioValues[i] = audioData[i];
+        }
+        reads++;
+        events.success(audioValues);
+      }
+    };
+  }
 }
+
