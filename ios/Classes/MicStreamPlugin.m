068a4db9be3fbf54345e113f766fc6c73934d09b
hungrymonkey <tedchang2010@gmail.com>
2019-06-30 20:10:35 -0700

    Change IOS to default swift plugin

--- ios/Classes/MicStreamPlugin.m
+++ ios/Classes/MicStreamPlugin.m
@@ -1,20 +1,8 @@
 #import "MicStreamPlugin.h"
+#import <mic_stream/mic_stream-Swift.h>
 
 @implementation MicStreamPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"mic_stream"
-            binaryMessenger:[registrar messenger]];
-  MicStreamPlugin* instance = [[MicStreamPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  [SwiftMicStreamPlugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
